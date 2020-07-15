8d9c03ec18d43df18013be28ba1c93dfbe36d1e8
Nick <nikatabata22@gmail.com>
2020-07-15 13:35:31 +0400

    Add project files.

---
+++ RabbitMq.Send/Program.cs
@@ -0,0 +1,45 @@
+﻿using Newtonsoft.Json;
+using RabbitMQ.Client;
+using System;
+using System.Text;
+
+namespace RabbitMq.Send
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.ExchangeDeclare("My.TestExchange", ExchangeType.Topic, true, false, null);
+
+                channel.QueueDeclare(queue: "My.TestQueue",
+                                     durable: false,
+                                     exclusive: false,
+                                     autoDelete: false,
+                                     arguments: null);
+
+                string message = JsonConvert.SerializeObject(new Student());
+                var body = Encoding.UTF8.GetBytes(message);
+
+                channel.BasicPublish(exchange: "My.TestExchange",
+                                     routingKey: "TestRouting",
+                                     basicProperties: null,
+                                     body: body);
+                Console.WriteLine(" [x] Sent {0}", message);
+            }
+
+            Console.WriteLine(" Press [enter] to exit.");
+            Console.ReadLine();
+        }
+    }
+
+    class Student
+    {
+        public string Name { get; set; } = "Nika";
+        public string LastName { get; set; } = "Tabatadze";
+        public int Age { get; set; } = 21;
+    }
+}
---
+++ RabbitMq/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+
+namespace RabbitMq
+{
+    class Program
+    {
+        private static ConnectionFactory _factory;
+        private static IConnection _connection;
+        private static IModel _model;
+
+        private const string ExchangeName = "Rabbit.Test.Events";
+        private const string QueueName = "TestQueue";
+
+        static void Main(string[] args)
+        {
+            var newMessage1 = "this is test message 1";
+            var newMessage2 = "this is test message 2";
+            var newMessage3 = "this is test message 3";
+
+            CreateQueue();
+
+            SendMessage(newMessage1);
+            SendMessage(newMessage2);
+            SendMessage(newMessage3);
+        }
+
+        private static void CreateQueue()
+        {
+            _factory = new ConnectionFactory() { HostName = "localhost", UserName = "guest", Password = "guest" };
+            _connection = _factory.CreateConnection();
+            _model = _connection.CreateModel();
+
+            _model.QueueDeclare(QueueName, true, false, false, null);
+        }
+
+        private static void SendMessage(string message)
+        {
+            _model.BasicPublish("", QueueName, null, Encoding.UTF8.GetBytes(message));
+        }
+    }
+}
